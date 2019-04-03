f971fe0404f02c5c95323551c05ee5ae6c84de15
Lydia Dalrymple <dalrymple.lydia@gmail.com>
2019-04-03 20:20:35 +0100

    adding sheets for website

---
+++ js/homepage.js
@@ -0,0 +1,5 @@
+function addTogether(oneThing, anotherThing) {
+  {console.error(anotherThing)}
+var newThing = oneThing + anotherThing
+alert(oneThing + " + " + anotherThing + " = " + newThing)}
+addTogether (2,2)

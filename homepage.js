5d9946f113121533a0a76d739bd5972b6566aaff
Lydia Dalrymple <dalrymple.lydia@gmail.com>
2019-04-03 20:27:42 +0100

    All the sheets

--- homepage.js
+++
@@ -1,5 +0,0 @@
-function addTogether(oneThing, anotherThing) {
-  {console.error(anotherThing)}
-var newThing = oneThing + anotherThing
-alert(oneThing + " + " + anotherThing + " = " + newThing)}
-addTogether (2,2)
